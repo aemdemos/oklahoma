2551bcb7df3fe49686ff6ef1ff59fa4ba0f9a2e5
Karl Pauls <kpauls@adobe.com>
2025-05-12 18:21:25 +0200

    feat: implement the footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,5 +16,16 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  // add back to top link
+  const backToTop = document.createElement('a');
+  backToTop.className = 'back-to-top';
+  backToTop.textContent = 'Back to top ^';
+  backToTop.href = '#';
+  backToTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  footer.querySelector('.section:nth-of-type(1)').appendChild(backToTop);
+
   block.append(footer);
 }
